44d1681d78c0422264e6b5eed783fb27e2a4c069
ao <ao@DESKTOP-VG1VM5R>
2016-11-21 09:09:57 +0900

    コードの整理

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,56 @@
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
 	static final String DATE_FORMAT = "yyyy/MM/dd HH:mm:ss";
-	 
+
 	public static void main(String[] args) throws Exception {
 
-    	SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
-    	
-    	
-    	//ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
-    	Timer timer = new Timer(true);
-    	
-    	timer.scheduleAtFixedRate(new SampleTask(Calendar.getInstance()),  0, 5000);
+		//TimerのAtFixedRateを試す
+		Timer timer = new Timer(true);
+		timer.scheduleAtFixedRate(new SampleTask(Calendar.getInstance()), 0, 5000);
 
-    	
-    	while(true){
-    		
-    	}
-    }
-	
+		//処理を永続化させる
+		while (true) {}
+	}
 
 }
 
 class SampleTask extends TimerTask {
-	 
+
+	//スケジュール開始時間
 	Calendar start = null;
-	SampleTask(Calendar start){
+
+	SampleTask(Calendar start) {
 		this.start = start;
 	}
-	
-    public void run() {
-    	//System.out.println("(ヽ´ω`)");
-        String output = "Starttime:";
-    	
-    	//現在日時取得
-        Calendar c = Calendar.getInstance();
 
-        //フォーマットを指定
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
-        output += sdf.format(c.getTime());
-        
-        Random rnd = new Random();
+	public void run() {
+		String output = "Starttime:";
 
-        int ran = rnd.nextInt(50000);
-        //重い処理
-        String str = "";
-        for(int i = 0; i < ran; i++){
-        	str += String.valueOf(i);
-        }
-        
-    	//現在日時取得
-        Calendar c2 = Calendar.getInstance();
-        output += "　　Endtime:" + sdf.format(c2.getTime()) + "  Rand:" + String.valueOf(ran) + "  経過時間：" + String.valueOf(c2.getTimeInMillis() - c.getTimeInMillis()) + "    開始からの経過時間：" + String.valueOf(c.getTimeInMillis() - start.getTimeInMillis()) ;
+		// 開始時間を取得
+		Calendar c = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+		output += sdf.format(c.getTime());
 
-        System.out.println(output);
-    	
-    }
+		// 重い処理（ランダム回数処理を行う）
+		Random rnd = new Random();
+		int ran = rnd.nextInt(50000);
+		String str = "";
+		for (int i = 0; i < ran; i++) {
+			str += String.valueOf(i);
+		}
+
+		//結果を出力
+		Calendar c2 = Calendar.getInstance();
+		output += "　　Endtime:" + sdf.format(c2.getTime()) + "  Rand:" + String.valueOf(ran) 
+				+ "  経過時間：" + String.valueOf(c2.getTimeInMillis() - c.getTimeInMillis()) 
+				+ "    開始からの経過時間：" + String.valueOf(c.getTimeInMillis() - start.getTimeInMillis());
+
+		System.out.println(output);
+
+	}
 }
